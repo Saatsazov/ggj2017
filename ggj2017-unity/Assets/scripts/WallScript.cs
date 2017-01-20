b78d122e4f6254356ef29db2c52c2c4bf1085263
Helen Koval <elena.koval.ukraine@gmail.com>
2017-01-21 01:06:26 +0200

    Save level data, re-factor y position update

--- ggj2017-unity/Assets/scripts/WallScript.cs
+++ ggj2017-unity/Assets/scripts/WallScript.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallScript : MonoBehaviour {
-    private float kAmplitude = 1f;
-    private float kAmplitudePrev = 1f;
+    private const int numberOfLevels = 27;
+    private const int heroLevelIndex = 13;
+    private Queue<float> levelsList = new Queue<float>();
+
+    private float kAmplitude = 1;
+    private float kAmplitudePrev = 1;
     private float previousAngle = 0f;
     private float epsilan = Mathf.PI / 36;
     private float amplitudeStep = 0.01f;
 
     Rigidbody2D body;
-	float x;
+
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
-	}
+        GenerateLevelsList();
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        AddNewLevel();
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        body.MovePosition(new Vector2(body.position.x, GetHeroLevel()));
+    }
+
+    private float NextAmplitude()
+    { 
         var random = new System.Random();
 
         if (Mathf.Abs(previousAngle - Mathf.PI) < epsilan)
@@ -37,16 +53,28 @@ public class WallScript : MonoBehaviour {
             previousAngle = 0;
         }
 
-        x += 0.02f;
-		var y = kAmplitudeLocal * Mathf.Sin(previousAngle);
-//		var pos = body.position;
-//		pos.y = Mathf.Sin (x);
-//		body.position = pos;
+        return kAmplitudeLocal * Mathf.Sin(previousAngle);
+    }
 
-		body.MovePosition(new Vector2(body.position.x, y));
+    private void GenerateLevelsList()
+    {
+        for(var i = 0; i < numberOfLevels; ++i)
+        {
+            levelsList.Enqueue(NextAmplitude());
+        }
+    }
 
-//		var pos = transform.position;
-//		pos.y = Mathf.Sin (x);
-//		transform.position = pos;
-	}
+    private void AddNewLevel()
+    {
+        levelsList.Enqueue(NextAmplitude());
+        levelsList.Dequeue();
+    }
+
+    private float GetHeroLevel()
+    {
+        var enumerator = levelsList.GetEnumerator();
+        var i = 0;
+        while (i++ < numberOfLevels) enumerator.MoveNext();
+        return enumerator.Current;
+    }
 }
