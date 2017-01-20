44bdbf4064efab7dbe5f5b1c04802dc70becd0ec
Bagrat Saatsazov <bagrat@saatsazov.com>
2017-01-20 23:11:09 +0200

    start adding wave

---
+++ ggj2017-unity/Assets/scripts/WallScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour {
+
+	Rigidbody2D body;
+	float x;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		x += 0.02f;
+		var y = Mathf.Sin (x);
+//		var pos = body.position;
+//		pos.y = Mathf.Sin (x);
+//		body.position = pos;
+
+		body.MovePosition(new Vector2(body.position.x, y));
+
+//		var pos = transform.position;
+//		pos.y = Mathf.Sin (x);
+//		transform.position = pos;
+	}
+}
