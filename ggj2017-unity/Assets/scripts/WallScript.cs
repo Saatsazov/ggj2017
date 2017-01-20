9557bcb9e7c7c8a4edd0a432da4063ee25dd6125
Helen Koval <elena.koval.ukraine@gmail.com>
2017-01-21 00:01:44 +0200

    Board y position implementation using sin with random amplitude

--- ggj2017-unity/Assets/scripts/WallScript.cs
+++ ggj2017-unity/Assets/scripts/WallScript.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallScript : MonoBehaviour {
+    private float kAmplitude = 1f;
+    private float kAmplitudePrev = 1f;
+    private float previousAngle = 0f;
+    private float epsilan = Mathf.PI / 36;
+    private float amplitudeStep = 0.01f;
 
-	Rigidbody2D body;
+    Rigidbody2D body;
 	float x;
 	// Use this for initialization
 	void Start () {
@@ -13,9 +18,27 @@ public class WallScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        var random = new System.Random();
 
-		x += 0.02f;
-		var y = Mathf.Sin (x);
+        if (Mathf.Abs(previousAngle - Mathf.PI) < epsilan)
+        {
+            kAmplitudePrev = kAmplitude;
+            kAmplitude = (float)random.NextDouble() + 0.3f;
+        }
+
+        var kAmplitudeLocal = Mathf.Abs(kAmplitude - kAmplitudePrev) < epsilan ?
+            kAmplitude : kAmplitudePrev + ((kAmplitude > kAmplitudePrev) ? amplitudeStep : -amplitudeStep);
+        kAmplitudePrev = kAmplitudeLocal;
+
+        previousAngle += Mathf.PI / 36; // + 5deg
+
+        if (Mathf.Abs(previousAngle - 2 * Mathf.PI) < epsilan)
+        {
+            previousAngle = 0;
+        }
+
+        x += 0.02f;
+		var y = kAmplitudeLocal * Mathf.Sin(previousAngle);
 //		var pos = body.position;
 //		pos.y = Mathf.Sin (x);
 //		body.position = pos;
