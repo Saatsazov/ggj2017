22db65ae533480d14baee239970f0d9d447a22f5
Bagrat Saatsazov <bagrat@saatsazov.com>
2017-01-20 21:57:00 +0200

    add menu & main game & implement space handling

---
+++ ggj2017-unity/Assets/scripts/GameMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour {
+
+
+	public GameObject pausePanel;
+	bool isPaused;
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (isPaused) {
+				
+				resume ();
+			} else {
+				pause ();
+			}
+		}
+	}
+
+	public void pause()
+	{
+		isPaused = true;
+		pausePanel.SetActive (true);
+	}
+
+	public void resume()
+	{
+		isPaused = false;
+		pausePanel.SetActive (false);
+	}
+}
---
+++ ggj2017-unity/Assets/scripts/GoToScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour {
+
+	public void goToSceneById(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void goToSceneByName(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
