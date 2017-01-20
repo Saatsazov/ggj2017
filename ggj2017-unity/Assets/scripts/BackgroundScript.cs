0670478f9a686f3733fdf5fcb0b856f1eb09f911
Bagrat Saatsazov <bagrat@saatsazov.com>
2017-01-20 22:45:17 +0200

    add temp background

---
+++ ggj2017-unity/Assets/scripts/BackgroundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour {
+
+	public float speed;
+	public float maxX = 100;
+
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+
+		pos.x += speed;
+
+		if (pos.x > maxX) {
+			pos.x -= maxX;
+		}
+
+		transform.position = pos;
+	}
+}
