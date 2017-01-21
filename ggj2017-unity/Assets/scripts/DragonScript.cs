5259474e611fc896f8c5ca88071b3a2670b70c9d
Bagrat Saatsazov <bagrat@saatsazov.com>
2017-01-21 14:40:05 +0200

    add dragon

---
+++ ggj2017-unity/Assets/scripts/DragonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonScript : MonoBehaviour {
+
+	public float speed = 0.01f;
+	public WaveGenerator waveGenerator;
+
+	GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = transform.position;
+		pos.x -= speed;
+		pos.y -= waveGenerator.getWorldHightByX (camera.transform.position.x) * 0.03f;
+		transform.position = pos;
+	}
+}
