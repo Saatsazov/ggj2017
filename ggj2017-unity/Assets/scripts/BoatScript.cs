6c51b4453754083a52a9c47a9e8b962381c49fb5
Bagrat Saatsazov <bagrat@saatsazov.com>
2017-01-22 02:25:10 +0200

    fix death anim

--- ggj2017-unity/Assets/scripts/BoatScript.cs
+++ ggj2017-unity/Assets/scripts/BoatScript.cs
@@ -11,10 +11,8 @@ public class BoatScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
-
-
 	}
-
+		
 	// Update is called once per frame
 	void Update () {
 		
